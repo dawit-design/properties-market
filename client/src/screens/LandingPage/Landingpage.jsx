4c11e28fd9697fb0c9e9f0616abf3ee0df9433f1
Bryan Molk <brybrym92@MacBook-Pro-3.local>
2021-04-05 16:01:53 -0400

    landing page fix

--- client/src/screens/LandingPage/Landingpage.jsx
+++ client/src/screens/LandingPage/Landingpage.jsx
@@ -6,23 +6,19 @@ import banner from "../img/images/bg-img.jpeg"
 import './LandingPage.css'
 
 export default function LandingPage() {
-	return (
-		<>
-		
-			<section className="landing-section-container">
-				<div className="container-land">
-					<div className="row">
-						<div className="col-lg-6">
-							<div className="about-text">
-								<div className="at-title">
-                  <h2 className="layout-welcome">Welcome to Realtors-inc</h2>
-									<p className='layout-ptag'>
-										Lorem Ipsum has been the industry’s standard dummy text ever
-										since the 1500s, when an unknown printer took a galley of
-										type.
+  return (
+    
+      <section className="landing-section-container">
+        <div className="container-land">
+          <div className="landing-top">
+            <h2 className="layout-welcome">Welcome to Realtors-inc</h2>
+            <p className='landing-ptag'>
+              Lorem Ipsum has been the industry’s standard dummy text ever
+              since the 1500s, when an unknown printer took a galley of
+              type.
 									</p>
-								</div>
-								<div className="at-feature">
+          </div>
+          <div className="at-feature">
 									{data.map((item) => {
 										const { id, img, title, text } = item;
 										return (
@@ -31,26 +27,19 @@ export default function LandingPage() {
                           <img className="af-icon" src={img} alt="" />
 												</div>
 												<div className="af-text">
-													<h6>{title}</h6>
-													<p>{text}</p>
+													<h6 className="landing-title">{title}</h6>
+													<p className="landing-text">{text}</p>
 												</div>
 											</div>
 										);
 									})}
 								</div>
-							</div>
-						</div>
-						<div class="col-lg-6">
-              <div class="landing-pic set-bg">
-                <Link to= '/landing'>
+            <div class="landing-pic set-bg">
+              <Link to='/landing'>
                 <img src={aboutImg} className="landing-img-bottom" />
-                </Link>
-								
-							</div>
-						</div>
-					</div>
+              </Link>
+            </div>         
 				</div>
-			</section>
-		</>
+    </section>
 	);
 }
